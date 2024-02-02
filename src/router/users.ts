34678458f924a64061e9286eba746f7db78e75bd
vincent <126578635+VincentDub2@users.noreply.github.com>
2024-02-02 22:15:59 +0100

    -/emails/verify est passé en post

--- src/router/users.ts
+++ src/router/users.ts
@@ -5,7 +5,7 @@ import upload from "../middlewares/uploadMiddleware";
 import volunteerToFestivalController from "../controllers/volunteerToFestival.controller";
 
 export default (router: express.Router) => {
-    router.get('/emails/verify',userController.verifyEmail)
+    router.post('/emails/verify',userController.verifyEmail)
     router.get('/users/current',middleware.isAuthenticated,userController.getCurrentUser)
     router.put('/users/profile-picture',middleware.isAuthenticated,middleware.isEmailVerified,upload.single('picture'),userController.updateUserPicture)
     router.put('/users/:id',middleware.isAuthenticated,middleware.isAccountOwner,userController.updateUser);
