385979df6fe99ec0358c3c307e8318f7e458b2a4
vincent <126578635+VincentDub2@users.noreply.github.com>
2024-02-03 22:59:02 +0100

    -Upload csv marche sur vercel

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -35,81 +35,32 @@ const csvService = {
      * Process and save game data from CSV file
      * Il est recommandé d'utiliser Promise.all() pour paralléliser le traitement des données
      * Il est beaucoup plus rapide de traiter les données en parallèle que de traiter les données une par une
-        * @param csvData
+     * @param csvData
      * @returns {Promise<void>}
      *
      */
     async processAndSaveGameData2(csvData : any[]) {
         console.time('TraitementTotal');
         const existingGames = new Set((await prisma.jeux.findMany()).map(game => game.idGame));
-    
+
         const batchSize = 500; // Taille de chaque groupe
-        const createdGames = [];
-    
+        // Par exemple, si csvData contient 1000 éléments et batchSize = 100, alors nous aurons 10 groupes
         for (let i = 0; i < csvData.length; i += batchSize) {
             const batch = csvData.slice(i, i + batchSize);
-            const batchResult = await Promise.all(batch.map(data => this.processGame(transformToGameData(data), existingGames)));
-            createdGames.push(...batchResult.flat());
+            const promises = batch.map(data => this.processGame(transformToGameData(data)));
+            await Promise.all(promises);
         }
-        console.timeEnd('TraitementTotal');
-        return createdGames.flat();
-    },
-    async processGame(data : GameData, existingGames : Set<number>) {
-        try {
-            const idGame = data.idGame;
-            if (existingGames.has(idGame)) {
-                const createdGame = await prisma.jeux.update({
-                    where: { idGame },
-                    data: data
-                });
-                return createdGame.idGame; // Return the updated idGame
-            } else {
-                const createdGame = await prisma.jeux.create({
-                    data: data
-                });
-                return createdGame.idGame; // Return the created idGame
-            }
-        } catch (error) {
-            logger.error(`Erreur lors de l'enregistrement du jeu ${data.name}:`, error);
-            return null; // Return null for failed creations
-        }
-    },
 
-    async processGame2(data: GameData) {
+        console.timeEnd('TraitementTotal');
+    },
+    processGame(data: GameData) {
+        // Directement retourner l'opération Prisma ici sans `await`
         return prisma.jeux.upsert({
             where: { idGame: data.idGame },
             update: data,
             create: data,
         });
     },
-    async processAndSaveGameData3(csvData: any[]) {
-        console.time('TraitementTotal');
-        let createdGames = [];
-        const batchSize = 100;
-
-        for (let i = 0; i < csvData.length; i += batchSize) {
-            const batch = csvData.slice(i, i + batchSize);
-            const operations = batch.map(data => {
-                // Transformez les données ici
-                const gameData = transformToGameData(data);
-                // Retournez l'opération upsert directement sans l'utiliser avec `await`
-                return prisma.jeux.upsert({
-                    where: { idGame: gameData.idGame },
-                    update: gameData,
-                    create: gameData,
-                });
-            });
-
-            // Exécutez toutes les opérations de ce lot dans une seule transaction
-            const batchResult = await prisma.$transaction(operations);
-            createdGames.push(...batchResult);
-        }
-        console.timeEnd('TraitementTotal');
-        return createdGames; // Ce sera un tableau des résultats de toutes les opérations upsert
-    },
-
-
-
     /**
      * Process and save game data from CSV file
      * Cette methode est plus lente que la methode processAndSaveGameData2
@@ -133,8 +84,8 @@ const csvService = {
                     await prisma.jeux.create({
                         data: game
                     })
-                logger.info(`Jeu ${game.name} enregistré avec succès`)
-                ;}
+                    logger.info(`Jeu ${game.name} enregistré avec succès`)
+                    ;}
             } catch (error) {
                 logger.error(`Erreur lors de l'enregistrement du jeu ${data['Nom du jeu']}:`, error);
             }
