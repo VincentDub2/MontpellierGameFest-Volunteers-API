f6954c54e53591cd5a3d4c4fea66cbf426a73239
vincent <cerise.litchis.0z@icloud.com>
2024-03-17 11:12:48 +0100

    -Enlever verification email

--- src/router/users.ts
+++ src/router/users.ts
@@ -8,7 +8,7 @@ export default (router: express.Router) => {
     router.post('/emails/verify',userController.verifyEmail)
     router.get('/users/current',middleware.isAuthenticated,userController.getCurrentUser)
     router.get('/users/:id', userController.getUser);
-    router.put('/users/profile-picture',middleware.isAuthenticated,middleware.isEmailVerified,upload.single('picture'),userController.updateUserPicture)
+    router.put('/users/profile-picture',middleware.isAuthenticated,upload.single('picture'),userController.updateUserPicture)
     router.put('/users/:id',middleware.isAuthenticated,middleware.isAccountOwner,userController.updateUser);
     router.get('/users/:volunteerId/festivals', volunteerToFestivalController.getFestivalsByVolunteer);
 
